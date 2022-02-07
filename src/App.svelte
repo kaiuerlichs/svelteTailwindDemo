261bcaf430298b0b8de3b6ca42151d2ef3e297ca
Kai Uerlichs <2421101@dundee.ac.uk>
2022-02-07 02:37:30 +0000

    test

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,11 @@
 	export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="grid grid-cols-4 gap-4">
+	{#each {length: 5} as _, i}
+  		<div>{i+1}</div>
+	{/each}
+</div>
 
 <style global lang="postcss">
 	@tailwind base;
